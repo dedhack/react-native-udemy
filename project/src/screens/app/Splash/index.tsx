f0b9d945dfaeacd96e9f49b321f4e3ed0d6651f7
Izhar <eezhar@gmail.com>
2023-06-01 14:12:11 +0800

    create splash screen

--- project/src/screens/app/Splash/index.tsx
+++ project/src/screens/app/Splash/index.tsx
@@ -1,16 +1,25 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+
+import {styles} from './styles';
 
 type Props = {};
 
 const Splash = (props: Props) => {
   return (
-    <View>
-      <Text>You'll Find All You Need Here!</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        resizeMode="contain"
+        source={require('../../../assets/Splash.png')}
+      />
+      <Text style={styles.title}>You'll Find </Text>
+      <Text style={[styles.title, styles.innerText]}>All You Need </Text>
+      <Text style={styles.title}>Here!</Text>
     </View>
   );
 };
 
 export default Splash;
 
-const styles = StyleSheet.create({});
+// const styles = StyleSheet.create({});
---
+++ project/src/screens/app/Splash/styles.ts
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  innerText: {
+    color: '#FCA34D',
+    textDecorationLine: 'underline',
+  },
+});
